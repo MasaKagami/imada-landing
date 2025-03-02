47de60f8a079538c42d898047aab9cf61765c1dd
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2025-03-02 09:52:58 -0500

    updated margins

--- src/app/about/agencies.tsx
+++ src/app/about/agencies.tsx
@@ -6,9 +6,9 @@ const Agencies = () => {
             <div className="hidden sm:block absolute w-1/2 inset-0 left-1/2 bg-[url('/about2.jpg')] bg-cover bg-center"></div>
 
             <div className="sm:hidden flex w-full h-[300px] bg-[url('/about2.jpg')] bg-cover bg-center"></div>
-            <div className="relative w-full h-full flex sm:flex-row flex-col  sm:py-0 py-10 justify-between items-center max-w-[90%] sm:max-w-[75%] m-auto">
-                <div className="sm:w-1/2 w-full flex flex-col gap-8 px-1 sm:px-8 lg:px-10 xl:px-16 items-start">
-                    <h1 className="text-start text-4xl sm:text-5xl font-bold">
+            <div className="relative w-full h-full flex sm:flex-row flex-col  sm:py-0 py-10 justify-between items-center max-w-[90%] sm:max-w-[80%] m-auto">
+                <div className="sm:w-1/2 w-full flex flex-col gap-8 px-1 sm:px-8 lg:px-10 xl:px-12 items-start">
+                    <h1 className="text-start text-4xl sm:text-4xl lg:text-5xl font-bold">
                         Subordinate Agencies
                     </h1>
                     <p>Under our subordinate agencies, including Luen Wah (HK) Medicine Limited. , Imada Luen Wah World Limited, Imada Luen Wah Medicine (S) Pte Limited and multiple subsidiaries. Imada owns the development of its business, with product manufacture, license processing, management agents, patent and trademark protection, quality control and marketing to ensure credibility at its highest level.</p>
--- src/app/about/trademark.tsx
+++ src/app/about/trademark.tsx
@@ -6,10 +6,10 @@ const Trademark = () => {
             <div className="hidden sm:block absolute w-1/2 inset-0 left-1/2 "></div>
 
             <div className="sm:hidden flex w-full h-[300px] bg-[url('/about1.jpg')] bg-cover bg-center"></div>
-            <div className="relative w-full h-full flex sm:flex-row flex-col  sm:py-0 py-10 justify-between items-center max-w-[90%] sm:max-w-[75%] m-auto">
+            <div className="relative w-full h-full flex sm:flex-row flex-col  sm:py-0 py-10 justify-between items-center max-w-[90%] sm:max-w-[80%] m-auto">
                 <div className="sm:w-1/2 w-full"></div>
-                <div className="sm:w-1/2 w-full flex flex-col gap-8 px-1 sm:px-8 lg:px-10 xl:px-16">
-                    <h1 className="text-start text-4xl sm:text-5xl font-bold">Trademark and Companies</h1>
+                <div className="sm:w-1/2 w-full flex flex-col gap-8 px-1 sm:px-8 lg:px-10 xl:px-12">
+                    <h1 className="text-start text-4xl sm:text-4xl lg:text-5xl font-bold">Trademark and Companies</h1>
                     <p>Imada, in response to the protection of trademarks and patents of each product, the establishment of this comprehensive limited company began in the 1950s with more than 70 years of history to date. Products are marketed around the world with the trust and love from foreign people, young to elderly, and they are advised to use safely as home and travel medicine.</p>
                 </div>
 
